0f47b22a0225c071bd09f7ffd44e18cf6d395616
Renan <67446339+MrCoffeeOri@users.noreply.github.com>
2022-08-23 00:24:44 -0300

    Delete helpers directory

--- helpers/resetDB.js
+++
@@ -1,4 +0,0 @@
-import { readdir, writeFile } from 'fs/promises'
-
-const _path = "C:\\Users\\Pichau\\OneDrive\\Área de Trabalho\\Projects\\NodeChat\\database";
-(await readdir(_path)).forEach(file => writeFile(`${_path}\\${file}`, file == "logs.json" ? '[]' : '{}'))
