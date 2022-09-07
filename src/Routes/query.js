dfb7a91cf7f19f6fa2f6ea25d6e4b8dc1513358e
Renan Penha Marquette <rpenhamarquette@gmail.com>
2022-09-07 15:19:52 -0300

    Query fixed

--- src/Routes/query.js
+++ src/Routes/query.js
@@ -11,10 +11,8 @@ queryRouter.get("/:table/:limit/:chars", async (req, res) => {
         
     if (isNaN(req.params.limit) || req.params.limit < 0)
         return res.status(400).json({ message: "Limit is not a number or is negative" })
-
-    if (req.params.limit > queryTable.length)
-        req.params.limit = queryTable.length
-
+    
+    req.params.limit = req.params.limit > queryTable.length ? queryTable.length : req.params.limit
     let query = []
     queryTable.forEach(item => {
         if (item.name.includes(req.params.chars) || item.name.includes(req.params.chars.toLowerCase()) || item.name.includes(req.params.chars.toUpperCase())) {
@@ -24,8 +22,5 @@ queryRouter.get("/:table/:limit/:chars", async (req, res) => {
             { id: item.id, name: item.name, creationDate: item.creationDate, inviteToken: item.inviteToken, groups: item.groups && Object.keys(item.groups).map(groupID => ({ name: groups.data[groupID].name, creationDate: groups.data[groupID].creationDate })), users: item.users && Object.values(item.users) })
         }
     })
-    if (query.length <= 0)
-        return res.status(404).json({ message: "Query data not found" })
-    
     res.status(200).json({ message: "Success", query: query.slice(0, req.params.limit), remaining: Math.max(0, query.length - req.params.limit) })
 })
