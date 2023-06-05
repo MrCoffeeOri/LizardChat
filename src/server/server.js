8844d19ff5b3474865d82b0dfd65f1282704d92a
Renan <67446339+MrCoffeeOri@users.noreply.github.com>
2023-06-05 11:29:01 -0300

    Update server.js

--- src/server/server.js
+++ src/server/server.js
@@ -29,7 +29,7 @@ app.use("/api/chat", chatRouter)
 app.use("/api/user", userRouter)
 app.use("/api/query", queryRouter)
 app.use("/api/upload", uploadRouter)
-app.use(express.static('public'))
+app.use(express.static('src/public'))
 io.on('connection', async socket => {
     const checkError = document => (document?.errors?.message || !document) && socket.emit('error', { error: document?.errors?.message || "Invalid request"})
     const user = await User.FindByUID(socket.handshake.auth.userID)
